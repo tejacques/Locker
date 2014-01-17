e6c9a124cb5725a82f25806d8255af815ba88ea2
Tom Jacques <tejacques@gmail.com>
2014-01-16 23:23:28 -0500

    Initial Commit of Locker class

---
+++ src/Locker/Locker.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Locker
+{
+    public class Locker
+    {
+
+        /// <summary>
+        /// Performs an action while the provided ReaderWriterLockSlim is
+        /// read locked, while taking the necessary exception precautions.
+        /// </summary>
+        /// <param name="rwlock">The ReaderWriterLockSlim</param>
+        /// <param name="whileReadLocked">The action to perform while read locked</param>
+        public static void ReadLock(
+            ReaderWriterLockSlim rwlock,
+            Action whileReadLocked)
+        {
+            try
+            {
+                rwlock.EnterReadLock();
+                whileReadLocked();
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                rwlock.ExitReadLock();
+            }
+        }
+
+        /// <summary>
+        /// Performs an action while the provided ReaderWriterLockSlim is
+        /// write locked, while taking the necessary exception precautions.
+        /// </summary>
+        /// <param name="rwlock">The ReaderWriterLockSlim</param>
+        /// <param name="whileWriteLocked">The action to perform while write locked</param>
+        public static void WriteLock(
+            ReaderWriterLockSlim rwlock,
+            Action whileWriteLocked)
+        {
+            try
+            {
+                rwlock.EnterWriteLock();
+                whileWriteLocked();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                rwlock.ExitWriteLock();
+            }
+        }
+    }
+}
